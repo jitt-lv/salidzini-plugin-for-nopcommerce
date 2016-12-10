a6a7e67ea65f970e1355faeaf746e02e68613fc2
Edgars <edgarsp@jitt.lv>
2016-12-10 21:53:19 +0200

    Working config view, refrences fix

---
+++ Models/ConfigurationModel.cs
@@ -0,0 +1,9 @@
+﻿using Nop.Web.Framework.Mvc;
+
+namespace Nop.Plugin.Feed.Salidzini.Models
+{
+    public class ConfigurationModel : BaseNopModel
+    {
+        public string MyProperty { get; set; }
+    }
+}
